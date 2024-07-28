9dcdc99d02d04ae00937dd988ee03f3aed153e43
Merlina <noahchukita@gmail.com>
2024-07-28 13:02:17 -0300

    feat(auth): creación del AuthSlice con Redux para la gestión de autenticación

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { authSlice } from './auth/authSlice'
 import { uiSlice } from './ui/uiSlice'
 import { calendarSlice } from './calendar/calendarSlice'
 
 export const store = configureStore({
     reducer: {
-        ui: uiSlice.reducer,
+        auth: authSlice.reducer,
         calendar: calendarSlice.reducer,
+        ui: uiSlice.reducer,
     },
     middleware: ( getDefaultMiddleware ) => getDefaultMiddleware({
         serializableCheck: false 
