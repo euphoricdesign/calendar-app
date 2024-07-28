a43fc7cf18b2e2a5455f1331e64d8200f7ba8327
Merlina <noahchukita@gmail.com>
2024-07-28 13:09:58 -0300

    feat: actualización del archivo principal de la app para utilizar datos desde la store

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,17 +2,38 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth'
 import { CalendarPage } from '../calendar'
+import { useEffect } from 'react'
+import { useAuth } from '../hooks/useAuth'
+
 
 export const AppRouter = () => {
 
-    // Algún tipo de validación, por ahora, temporal
-    const authStatus = 'authenticated'
+    const { status, checkAuthToken } = useAuth();
 
+    useEffect(() => {
+        checkAuthToken();
+    }, [])
+    
+
+
+    if ( status === 'checking' ) {
+        return (
+            <div className='tw-flex tw-justify-center tw-items-center tw-h-[100vh]'>
+                <div class="loadingspinner">
+                    <div id="square1"></div>
+                    <div id="square2"></div>
+                    <div id="square3"></div>
+                    <div id="square4"></div>
+                    <div id="square5"></div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <Routes>
             {
-                (authStatus === 'not-authenticated')
+                (status === 'not-authenticated')
                 ? <Route path="/auth/*" element={<LoginPage />} />
                 : <Route path="/*" element={<CalendarPage />} />
             }
