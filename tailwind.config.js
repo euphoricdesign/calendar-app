97d42cfbc206fb4f8e6c1af8927bee1f080dbd23
Merlina <noahchukita@gmail.com>
2024-07-29 11:49:23 -0300

    style: actualizar estilos generales de la app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0 2px 10px rgba(0, 0, 0, 0.1)',
+        // Puedes añadir más sombras personalizadas aquí
+      },
+    },
   },
   plugins: [],
 }
